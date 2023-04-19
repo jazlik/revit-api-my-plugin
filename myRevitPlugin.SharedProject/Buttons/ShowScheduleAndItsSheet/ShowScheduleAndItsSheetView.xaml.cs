ceba30d1834bad61f3fbcae4b48ae2f6124664ff
jazlik <jakubkozlik5@gmail.com>
2023-04-19 09:29:57 +0200

    Fixing problem with .xaml and .xaml.cs files that were not nested.

---
+++ myRevitPlugin.SharedProject/Buttons/ShowScheduleAndItsSheet/ShowScheduleAndItsSheetView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace myRevitPlugin
+{
+    /// <summary>
+    /// Interaction logic for ShowScheduleAndItsSheetView.xaml
+    /// </summary>
+    public partial class ShowScheduleAndItsSheetView
+    {
+        public ShowScheduleAndItsSheetView()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
