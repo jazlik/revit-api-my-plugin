17aa8276416c6e0e68d41401002977ca80d1f212
jazlik <jakubkozlik5@gmail.com>
2023-08-08 08:53:31 +0200

    Updating CopyParameterValueFromParameterToParameter method

--- myRevitPlugin.SharedProject/Buttons/CopyParameterValue/CopyParameterValueModel.cs
+++ myRevitPlugin.SharedProject/Buttons/CopyParameterValue/CopyParameterValueModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using Autodesk.Revit.DB;
@@ -130,51 +131,100 @@ namespace myRevitPlugin.Buttons.CopyParameterValue
             List<Element> elementsList = GetAllElementsOfGivenCategory(selectedCategory).ToElements().ToList();
             ElementParameterWrapper fromParameter = (elementParameterWrapperList1.Where(x => x.IsObjectSelected)).ElementAt(0);
             ElementParameterWrapper toParameter = (elementParameterWrapperList2.Where(x => x.IsObjectSelected)).ElementAt(0);
+            StorageType fromParameterStorageType = fromParameter.StorageType;
+            StorageType toParameterStorageType = toParameter.StorageType;
 
-            var fromParameterStorageType = fromParameter.StorageType;
-            var toParameterStorageType = toParameter.StorageType;
-
-            using (Transaction t = new Transaction(Doc, "Set Parameters"))
+            if (fromParameterStorageType == toParameterStorageType)
             {
-                t.Start();
-
-                foreach (Element element in elementsList)
+                using (Transaction t = new Transaction(Doc, "Set Parameters"))
                 {
-                    // Both paramters are shared paramters
-                    if (fromParameter.BuiltInParameterId == BuiltInParameter.INVALID && toParameter.BuiltInParameterId == BuiltInParameter.INVALID)
-                    {
-                        Parameter parameterToSet = element.get_Parameter(toParameter.GUID);
-                        var value = (element.get_Parameter(fromParameter.GUID)).AsString();
-                        parameterToSet.Set(value);
-                    }
-                    // Both paramters are BuiltInParameters
-                    else if (fromParameter.BuiltInParameterId != BuiltInParameter.INVALID && toParameter.BuiltInParameterId != BuiltInParameter.INVALID)
-                    {
-                        Parameter parameterToSet = element.get_Parameter(toParameter.BuiltInParameterId);
-                        var value = (element.get_Parameter(fromParameter.BuiltInParameterId)).AsString();
-                        parameterToSet.Set(value);
-                    }
-                    // fromParamter is shared while toParamter is BuiltInParameter
-                    else if (fromParameter.BuiltInParameterId == BuiltInParameter.INVALID && toParameter.BuiltInParameterId != BuiltInParameter.INVALID)
-                    {
-                        Parameter parameterToSet = element.get_Parameter(toParameter.BuiltInParameterId);
-                        var value = (element.get_Parameter(fromParameter.GUID)).AsString();
-                        parameterToSet.Set(value);
-                    }
-                    // fromParamter is BuiltInParamter while toParameter is shared
-                    else if (fromParameter.BuiltInParameterId != BuiltInParameter.INVALID && toParameter.BuiltInParameterId == BuiltInParameter.INVALID)
-                    {
-                        Parameter parameterToSet = element.get_Parameter(toParameter.GUID);
-                        var value = (element.get_Parameter(fromParameter.BuiltInParameterId)).AsString();
-                        parameterToSet.Set(value);
-                    }
-                }
+                    t.Start();
 
-                t.Commit();
+                    foreach (Element element in elementsList)
+                    {
+                        Parameter parameterToSet = default;
+                        Parameter parameterToSetFrom = default;
+                        var value = (dynamic)null;
+
+                        // Both paramters are shared paramters
+                        if (fromParameter.BuiltInParameterId == BuiltInParameter.INVALID && toParameter.BuiltInParameterId == BuiltInParameter.INVALID)
+                        {
+                            parameterToSet = element.get_Parameter(toParameter.GUID);
+                            parameterToSetFrom = element.get_Parameter(fromParameter.GUID);
+                        }
+                        // Both paramters are BuiltInParameters
+                        else if (fromParameter.BuiltInParameterId != BuiltInParameter.INVALID && toParameter.BuiltInParameterId != BuiltInParameter.INVALID)
+                        {
+                            parameterToSet = element.get_Parameter(toParameter.BuiltInParameterId);
+                            parameterToSetFrom = element.get_Parameter(fromParameter.BuiltInParameterId);
+                        }
+                        // fromParamter is shared while toParamter is BuiltInParameter
+                        else if (fromParameter.BuiltInParameterId == BuiltInParameter.INVALID && toParameter.BuiltInParameterId != BuiltInParameter.INVALID)
+                        {
+                            parameterToSet = element.get_Parameter(toParameter.BuiltInParameterId);
+                            parameterToSetFrom = element.get_Parameter(fromParameter.GUID);
+                        }
+                        // fromParamter is BuiltInParamter while toParameter is shared
+                        else if (fromParameter.BuiltInParameterId != BuiltInParameter.INVALID && toParameter.BuiltInParameterId == BuiltInParameter.INVALID)
+                        {
+                            parameterToSet = element.get_Parameter(toParameter.GUID);
+                            parameterToSetFrom = element.get_Parameter(fromParameter.BuiltInParameterId);
+                        }
+
+                        switch (fromParameterStorageType)
+                        {
+                            case StorageType.String:
+                                value = parameterToSetFrom.AsString();
+                                break;
+
+                            case StorageType.Integer:
+                                value = parameterToSetFrom.AsInteger();
+                                break;
+
+                            case StorageType.ElementId:
+                                value = parameterToSetFrom.AsElementId();
+                                break;
+
+                            case StorageType.Double:
+                                value = parameterToSetFrom.AsDouble();
+                                break;
+
+                            case StorageType.None:
+                                value = "_";
+                                break;
+                            default:
+                                value = "_";
+                                break;
+                        }
+
+                        if (value != null && value != "")
+                        {
+                            parameterToSet.Set(value);
+                        }
+
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    t.Commit();
+                }
+            }
+
+            else
+            {
+                // Creates a Revit task dialog to communicate error to the user, if StorageType of both parameters are not the same
+                TaskDialog popingErrorDialog = new TaskDialog("CopyParameterValue Error");
+                popingErrorDialog.MainInstruction = "Error occured while copying parameters value!";
+                string message = String.Format(
+                    "You cannot copy value from parameter \"{0}\" to parameter \"{1}\"." +
+                    "\n" +
+                    "Parameter \"{0}\" holds value that is a {2} type, while parameter \"{1}\" holds value that is a {3} type." +
+                    "\n" +
+                    "You can only copy value between parameters with the same value types.", fromParameter.Name, toParameter.Name, fromParameterStorageType, toParameterStorageType);
+                popingErrorDialog.MainContent = message;
+                popingErrorDialog.Show();
             }
         }
-
-        // Window pops up saying, are you sure you want to copy data from Parameter A to Parameter B? Click Yes or No
-        // Parameter data is copied
     }
 }
